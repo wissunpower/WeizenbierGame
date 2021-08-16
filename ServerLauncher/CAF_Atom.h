e92b01d7fe4e5b484538b0dfad21ff8739e36bac
wissunpower <30381759+wissunpower@users.noreply.github.com>
2021-08-16 20:33:48 +0900

    불필요한 메시지 atom 제거

--- ServerLauncher/CAF_Atom.h
+++ ServerLauncher/CAF_Atom.h
@@ -11,8 +11,6 @@ CAF_POP_WARNINGS
 
 CAF_BEGIN_TYPE_ID_BLOCK(server_launcher, first_custom_type_id)
 
-CAF_ADD_ATOM(server_launcher, send_wrappedmessage)
-
 CAF_ADD_ATOM(server_launcher, send_to_client_atom)
 CAF_ADD_ATOM(server_launcher, broadcast_atom)
 CAF_ADD_ATOM(server_launcher, broadcast_notification_atom)
